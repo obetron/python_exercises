4076c09d4d3e797d1c5cf871845780507eff0c3e
obetron <erenbasaran@gmail.com>
2019-06-20 08:36:10 +0300

    changes applied.

--- GetListExercises.py
+++ GetListExercises.py
@@ -9,4 +9,4 @@ last_color = color_list[-1]
 print('First Color of the list: ', first_color)
 print('Last Color of the list: ', last_color)
 print('First Color of the list is {0}, Last Color of the list is {1}'.format(first_color, last_color))
-print('irst Color of the list is %s, Last Color of the list is %s'%(color_list[0], color_list[-1]))
+print('First Color of the list is %s, Last Color of the list is %s'%(color_list[0], color_list[-1]))
