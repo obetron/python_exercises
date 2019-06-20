2fd7de5dcfd5694f2c921936eca27b9cc28ef619
obetron <erenbasaran@gmail.com>
2019-06-20 08:34:43 +0300

    Get list object exercises added.

---
+++ GetListExercises.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program to display the first and last colors from the following list. Go to the editor
+color_list = ["Red","Green","White" ,"Black"]
+"""
+
+color_list = ['Red', 'Green', 'White', 'Black']
+first_color = color_list[0]
+last_color = color_list[-1]
+print('First Color of the list: ', first_color)
+print('Last Color of the list: ', last_color)
+print('First Color of the list is {0}, Last Color of the list is {1}'.format(first_color, last_color))
+print('irst Color of the list is %s, Last Color of the list is %s'%(color_list[0], color_list[-1]))
